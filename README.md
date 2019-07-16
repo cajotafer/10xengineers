49f7ba64346e6a8f4c4835f4487b4c74478aff9b
Carlos Fernández <hello@cajotafer.com>
2019-07-16 10:29:43 -0500

    [UPDATE] - SEO improvement 10xEngineer -> 10x Engineers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Welcome to 10xEngineer Test
+## Welcome to 10x Engineers Test
 
 This is a one-day-made sarcastic test to know your "engineer level" based in the controversial [twitter thread of Shekhar Kirani](https://twitter.com/skirani/status/1149302828420067328) on `8:02 AM · Jul 11, 2019`, that was highly rejected by the community.
 
@@ -10,13 +10,13 @@ This is a one-day-made sarcastic test to know your "engineer level" based in the
 
 - [Symbo1lics Ideas posted the Shekar Kirani thread on images.](https://twitter.com/Symbo1ics/status/1150141085580218368)
 - [Sahil Mansuri ](https://twitter.com/SVMansuri/status/1150052033350660096)showed that this theory can be applied to different areas and created its version to find 10xSalesPerson.
-- [Jane Manchun Wong](https://twitter.com/wongmjane) built [a website](http://10x.engineer/) to explain what a 10xEngineer is. Amazing job by the way.
-- [Arora](https://twitter.com/AroraXD) built [this website](https://10xengineer.glitch.me/) to show us the truth about 10xEngineers.
+- [Jane Manchun Wong](https://twitter.com/wongmjane) built [a website](http://10x.engineer/) to explain what a 10x Engineer is. Amazing job by the way.
+- [Arora](https://twitter.com/AroraXD) built [this website](https://10xengineer.glitch.me/) to show us the truth about 10x Engineers.
 - [Tierney Cyren](https://twitter.com/bitandbang) made something a bit different, [an open resource to share what a 1xEngineer is.](https://1x.engineer/).
 
 ## You can contribute
 
-If you want to help making 10xEngineers Test better, just PR your idea, I am sure we can improve this test quality. The conributions are ruled by the [Code of Conduct](/cajotafer/10xengineers/blob/master/CODE_OF_CONDUCT.md) of the project.
+If you want to help making 10x Engineers Test better, just PR your idea, I am sure we can improve this test quality. The conributions are ruled by the [Code of Conduct](/cajotafer/10xengineers/blob/master/CODE_OF_CONDUCT.md) of the project.
 
 Let's make this world a better place to live together.
 
--- src/App.svelte
+++ src/App.svelte
@@ -87,119 +87,119 @@
   }
 </style>
 
-<div id="fb-root"></div>
+<div id="fb-root" />
 <main>
   {#if started === false}
-  <p>
-    <q cite="https://twitter.com/skirani/status/1149302828420067328">
-      If you have a 10x engineer as part of your first few engineers, you
-      increase the odds of your startup success significantly.
-    </q>
-    <br />
-    Shekhar Kirani
-  </p>
-  <h1>{landing.title}</h1>
-  <div class="btn-container flex spacing">
-    <button on:click="{handleStart}">{landing.button}</button>
-  </div>
-  {:else if finished === false && robot === false}
-  <h2>{questions[count].q}</h2>
-  <div class="btn-container flex spacing">
-    {#each questions[count].r as option, i}
-    <button
-      on:click="{handleClick}"
-      data-value="{option.value}"
-      data-validate="{option.robot}"
-    >
-      {option.text}
-    </button>
-    {/each}
-  </div>
-  {:else if finished === false && robot === true}
-  <h2>We just want to make sure 🤖</h2>
-  <form action="" method="POST" on:submit|preventDefault="{handleSubmit}">
-    <div id="recaptcha"></div>
-    <br />
-    <input type="submit" value="Submit" />
-  </form>
-  <script
-    src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-    defer
-  ></script>
-  {:else if finished === true}
-  <h1>{result}</h1>
-  <div class="flex">
-    <a
-      class="twitter-share-button"
-      href="https://twitter.com/intent/tweet"
-      data-size="large"
-      data-text="{share}"
-      data-url="https://10xengineers.netlify.com/"
-      data-hashtags="10xEngineer"
-      data-related="skirani, cajotafer, cvander, dhh, addyosmani"
-    >
-      Tweet
-    </a>
-    &nbsp;&nbsp;or&nbsp;&nbsp;
-    <div
-      class="fb-share-button"
-      data-href="https://10xengineers.netlify.com/"
-      data-layout="button"
-      data-size="large"
-    >
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2F10xengineers.netlify.com%2F&amp;src=sdkpreparse"
-        class="fb-xfbml-parse-ignore"
-      >
-        Share
-      </a>
+    <p>
+      <q cite="https://twitter.com/skirani/status/1149302828420067328">
+        If you have a 10x engineer as part of your first few engineers, you
+        increase the odds of your startup success significantly.
+      </q>
+      <br />
+      Shekhar Kirani
+    </p>
+    <h1>{landing.title}</h1>
+    <div class="btn-container flex spacing">
+      <button on:click={handleStart}>{landing.button}</button>
     </div>
-  </div>
-  <i class="spacing">
-    The origin of the 10xEngineer Test is
-    <a href="https://twitter.com/skirani/status/1149302828420067328"
-      >this thread of Shekhar Kirani</a
-    >
-    that was highly rejected by the community. If you don't know about this,
-    just take a look.
-  </i>
-  <script>
-    window.twttr = (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-      if (d.getElementById(id)) return t;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js, fjs);
+  {:else if finished === false && robot === false}
+    <h2>{questions[count].q}</h2>
+    <div class="btn-container flex spacing">
+      {#each questions[count].r as option, i}
+        <button
+          on:click={handleClick}
+          data-value={option.value}
+          data-validate={option.robot}>
+          {option.text}
+        </button>
+      {/each}
+    </div>
+  {:else if finished === false && robot === true}
+    <h2>We just want to make sure 🤖</h2>
+    <form action="" method="POST" on:submit|preventDefault={handleSubmit}>
+      <div id="recaptcha" />
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+    <script
+      src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+      defer>
 
-      t._e = [];
-      t.ready = function(f) {
-        t._e.push(f);
-      };
+    </script>
+  {:else if finished === true}
+    <h1>{result}</h1>
+    <div class="flex">
+      <a
+        class="twitter-share-button"
+        href="https://twitter.com/intent/tweet"
+        data-size="large"
+        data-text={share}
+        data-url="https://10xengineers.netlify.com/"
+        data-hashtags="10xEngineer"
+        data-related="skirani, cajotafer, cvander, dhh, addyosmani">
+        Tweet
+      </a>
+      &nbsp;&nbsp;or&nbsp;&nbsp;
+      <div
+        class="fb-share-button"
+        data-href="https://10xengineers.netlify.com/"
+        data-layout="button"
+        data-size="large">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2F10xengineers.netlify.com%2F&amp;src=sdkpreparse"
+          class="fb-xfbml-parse-ignore">
+          Share
+        </a>
+      </div>
+    </div>
+    <i class="spacing">
+      The origin of the 10x Engineers Test is
+      <a href="https://twitter.com/skirani/status/1149302828420067328">
+        this thread of Shekhar Kirani
+      </a>
+      that was highly rejected by the community. If you don't know about this,
+      just take a look.
+    </i>
+    <script>
+      window.twttr = (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0],
+          t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
 
-      return t;
-    })(document, "script", "twitter-wjs");
-  </script>
+        t._e = [];
+        t.ready = function(f) {
+          t._e.push(f);
+        };
 
-  <script>
-    (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  </script>
+        return t;
+      })(document, "script", "twitter-wjs");
+    </script>
+    <script>
+      (function(d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src =
+          "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+    </script>
   {/if}
 </main>
 <address class="spacing">
-  Created by <a href="https://cajotafer.com">Cajotafer</a> |
-  <a href="https://twitter.com/cajotafer">Twitter</a> | Contribute on
+  Created by
+  <a href="https://cajotafer.com">Cajotafer</a>
+  |
+  <a href="https://twitter.com/cajotafer">Twitter</a>
+  | Contribute on
   <a href="https://github.com/cajotafer/10xengineers">Github</a>
 </address>
